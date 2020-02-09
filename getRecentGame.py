6099390ae7bc6631d0b091842065337e01bae24a
Mark Oliver <mark.c.oliver@gmail.com>
2020-02-09 00:58:15 -0500

    use user's Download directory and TEMP_PARSE_GAME environment var for output directory

--- getRecentGame.py
+++ getRecentGame.py
@@ -4,13 +4,16 @@ import glob
 import os
 import re
 import subprocess
-
+from gi.repository import GLib
+downloads_dir = GLib.get_user_special_dir(
+    GLib.UserDirectory.DIRECTORY_DOWNLOAD)
 # from parseGame import parseGame
 
-list_of_files = glob.glob('/home/mark/Downloads/game-*.json')
+list_of_files = glob.glob(downloads_dir + '/game-*.json')
 latest_file = max(list_of_files, key=os.path.getctime)
-#print(re.match(r".+?/game\-(\d{7})", latest_file).group(1))
-subprocess.check_call("./parseGame.py " + latest_file + " > /media/mark/hard/Maze_Escape/parseGame-" +
+# print(re.match(r".+?/game\-(\d{7})", latest_file).group(1))
+tempDir = os.environ['TEMP_PARSE_GAME']
+subprocess.check_call("./parseGame.py " + latest_file + " > " + tempDir + "parseGame-" +
                       re.match(r".+?/game\-(\d{7})", latest_file).group(1) + ".txt", shell=True)
-subprocess.check_call("code -r " + "/media/mark/hard/Maze_Escape/parseGame-" +
+subprocess.check_call("code -r " + tempDir + "parseGame-" +
                       re.match(r".+?/game\-(\d{7})", latest_file).group(1) + ".txt", shell=True)
